8492fe3d3c5d1e914881ed06a0c19952b1e40a6e
Arina <Ukam.naletko@mail.ru>
2025-03-17 15:43:21 +0300

    correct

--- Calculator.js
+++ Calculator.js
@@ -1,50 +1,51 @@
-class Calculator{
-    static a = 0;
-    static b = 0;
+class Calculator {
+    #a
+    #b
+
     isInvalidNumber(num) {
-        return num === Infinity || num === null || num === undefined || Number.isNaN(num);
+        return typeof num !== 'number' || !isFinite(num);
     }
     constructor(a, b) {
+
         if (this.isInvalidNumber(a) || this.isInvalidNumber(b)) {
             throw new Error("Invalid numbers");
         }
-    Calculator.a = a;
-    Calculator.b = b;
-}
-setX(num1) {
-    if (this.isInvalidNumber(num1))throw new Error("Invalid numbers")
-    this.a=num1
-}
-setY(num2) {
-    if (this.isInvalidNumber(num2))throw new Error("Invalid numbers")
-    this.b=num2
-}
- static logSum=()=>
-{
-    return this.a+this.b
-}
-static logMul=()=>
-{
-    return this.a*this.b
-}
-static logSub=()=>{
-    let res=0;
-    (this.a<=this.b)?res+=this.b-this.a:res+=this.a-this.b
-    return res
-}
-static logDiv=()=> {
-    if (this.b===0) throw new Error("Infinity")
-    return this.a / this.b
-}
+        this.#a = a
+        this.#b = b
+
+    }
+    setX(num1) {
+        if (this.isInvalidNumber(num1)) throw new Error("Invalid numbers")
+        this.#a = num1
+    }
+    setY(num2) {
+        if (this.isInvalidNumber(num2)) throw new Error("Invalid numbers")
+        this.#b = num2
+    }
+    logSum = () => {
+        return this.#a + this.#b
+    }
+    logMul = () => {
+        return this.#a * this.#b
+    }
+    logSub = () => {
+        return this.#b - this.#a
+    }
+    logDiv = () => {
+        if (this.#b === 0) throw new Error("Infinity")
+        return this.#a / this.#b
+    }
 };
-const Calc=new Calculator(2,10)
-Calc.setX(2)
-Calc.setY(10)
+const calc = new Calculator(2, 10)
+calc.setX(2)
+calc.setY(10)
 
 
-const logSumRef = Calculator.logSum;
-console.log(logSumRef()); 
-const logMulRef = Calculator.logMul;
-console.log(logMulRef()); 
-console.log(Calculator.logSub()); 
-console.log(Calculator.logDiv()); 
+const logSumRef = calc.logSum.bind(calc);
+const logMulRef = calc.logMul.bind(calc);
+const logSubRef = calc.logSub.bind(calc);
+const logDivRef = calc.logDiv.bind(calc);
+console.log(logSumRef());
+console.log(logMulRef());
+console.log(logSubRef());
+console.log(logDivRef());
